629f68f78dbebda584330355d34c7a072682efff
pablofernandonavarro <pablofernandonavarro@gmail.com>
2022-04-14 09:21:17 -0300

    inicio

---
+++ controladores/plantilla.controlador.php
@@ -0,0 +1,15 @@
+<?php
+  include_once("url_config.php");
+
+class ControladorPlantilla{
+
+  public function ctrPlantilla(){
+
+      include_once ROOT."vistas/recursos/plantilla.php";
+
+  }
+
+
+}
+
+?>
---
+++ url_config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+define('ROOT', __DIR__."/");
+
+
+define('HTTP', ($_SERVER["SERVER_NAME"] == "localhost")
+   ? "http://localhost/cher_systemas/"
+   : "http://70.38.46.72/cher_systemas/"
+);
+
+
+?>
